0ae57a02e0057745518648b9576ff98c10867b5e
hiroyone <hiroki.yonetani@zalando.de>
2022-11-03 07:10:09 +0100

    Simple Channel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{sync::mpsc, thread};
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let msg = String::from("Hi!");
+        tx.send(msg).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received)
 }
